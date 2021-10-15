2a2f313069cc6f1c6677547af406b7891cd8e743
Andres Plehm <andres@diwait.com>
2021-10-15 19:13:16 -0300

    .

--- app/Http/Livewire/Plant.php
+++ app/Http/Livewire/Plant.php
@@ -3,11 +3,40 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Http;
+use Auth;
+use Twilio\Rest\Client;
 
 class Plant extends Component
 {
+    public $plantid;
+    public $data;
+
     public function render()
     {
         return view('livewire.plant');
+    }
+
+    public function show()
+    {
+        $response = Http::withToken(Auth::user()->bearer_token)
+        ->get('https://backend-farm.plantvsundead.com/get-plant-detail-v2?plantId='.$this->plantid);
+
+        $this->data = $response->collect();
+    }
+
+    public function notification()
+    {
+        $sid    = "ACfe3a14435a41ff7d4c9493197d9e6dac"; 
+        $token  = "[Redacted]"; 
+        $twilio = new Client($sid, $token);
+        
+        $message = $twilio->messages 
+                  ->create("whatsapp:+5491134214001", // to 
+                           array( 
+                               "from" => "whatsapp:+14155238886",       
+                               "body" => "Tenes cuervos en tu granja. Entra rapidamente antes que te barran el patio." 
+                           ) 
+                  ); 
     }
 }
--- resources/views/livewire/plant.blade.php
+++ resources/views/livewire/plant.blade.php
@@ -1,11 +1,36 @@
 <div>
-    <x-jet-authentication-card>
-        <x-slot name="logo"></x-slot>
+    @isset($data)
+
+    <div class="rounded overflow-hidden shadow-lg justify-center bg-gray-800 w-60 m-auto">
+        <img src="{{ $data['data']['plant']['iconUrl'] }}" class="max-h-40 m-auto" alt="">
+        
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2 text-center border text-gray-500">{{ ucwords($data['data']['plant']['stats']['type']) }}</div>
+            <p class="text-gray-500 text-base border text-center">
+                LE:{{ $data['data']['plant']['farmConfig']['le'] }}/{{ $data['data']['plant']['farmConfig']['hours'] }} Hours
+                
+            </p>
+            <p class="text-gray-500 text-base border text-center">
+                
+                {{$data['data']['plant']['farmConfig']['le']/$data['data']['plant']['farmConfig']['hours']}} each hour
+            </p>
+          </div>
+    </div>
+    @endisset
+
+    <div class="rounded overflow-hidden shadow-lg justify-center bg-gray-800 w-1/2 m-auto p-20 mt-3">
     <x-jet-label>
         Plant ID
     </x-jet-label>
-        <x-jet-input id="plant_id" class="block mt-1 w-full" type="text" name="plant_id" :value="old('plant_id')" required autofocus>
+        <x-jet-input id="plant_id" wire:model="plantid" class="block mt-1 w-full" type="search" name="plant_id" autofocus>
                 
         </x-jet-input>
-    </x-jet-authentication-card>
+        
+        <x-jet-button type="submit" wire:click="show" class="mt-3">
+            Search
+        </x-jet-button>
+        <x-jet-button type="submit" wire:click="notification" class="mt-3">
+            Notification
+        </x-jet-button>
+    </div>
 </div>
